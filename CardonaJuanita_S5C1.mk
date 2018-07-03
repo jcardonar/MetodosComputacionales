ae4dab61f267cfeedcfd4e28f41f1676fa8111fc
jcardonar <j.cardona@uniandes.edu.co>
2018-07-03 11:39:49 -0500

    primer intento mk

---
+++ CardonaJuanita_S5C1.mk
@@ -0,0 +1,10 @@
+#Mi primer MakeFile
+
+resultados.pdf : resultados.tex plotdatos.py plotdatos1.py
+	pdflatex resultados.tex
+plotdatos.py : makedatos.py
+	python makedatos.py > datos.dat
+plotdatos1.py : makedatos1.py
+	python makedatos1.py > datos1.dat
+
+
