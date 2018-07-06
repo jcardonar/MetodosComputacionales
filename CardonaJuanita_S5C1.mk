d36abf0c2a0ba898d13696ac9b7623820e951759
jcardonar <j.cardona@uniandes.edu.co>
2018-07-06 11:56:13 -0500

    Makefile

--- CardonaJuanita_S5C1.mk
+++ CardonaJuanita_S5C1.mk
@@ -1,7 +1,11 @@
 #Mi primer MakeFile
 
-resultados.pdf : resultados.tex plotdatos.py plotdatos1.py
+resultados.pdf : resultados.tex plot.pdf plot1.pdf
 	pdflatex resultados.tex
+plot.pdf : plotdatos.py
+	python plotdatos.py
+plot1.pdf : plotdatos1.py
+	python plotdatos1.py
 plotdatos.py : makedatos.py
 	python makedatos.py > datos.dat
 plotdatos1.py : makedatos1.py
